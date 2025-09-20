4491f3963951f85b1fcd240f65b9ae1895c3de33
explodi <phaser@tubatuba.net>
2025-09-20 19:21:32 +0200

    Add publishing to itch

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Lawyer Up Bitch!")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
